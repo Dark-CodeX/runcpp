02bb31c6cb9b7e384adb1dd2e0e81d07e5ac6937
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-28 00:02:35 +0530

    caller header file

---
+++ src/caller/caller.hh
@@ -0,0 +1,35 @@
+#ifndef CALLER_HH
+#define CALLER_HH
+
+#include <openutils/vector/vector.hh>
+#include <openutils/sstring/sstring.hh>
+#if defined __linux || defined __linux__ || defined linux || defined __unix || defined __unix__ || defined unix
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+namespace runcpp
+{
+#if defined __linux || defined __linux__ || defined linux || defined __unix || defined __unix__ || defined unix
+    class caller
+    {
+      private:
+        openutils::vector_t<char *> args;
+        pid_t pid;
+
+      public:
+        caller(const openutils::vector_t<openutils::sstring> &arg);
+        void init();
+        ~caller();
+    };
+#else
+    class caller
+    {
+    };
+#endif
+}
+
+#endif
