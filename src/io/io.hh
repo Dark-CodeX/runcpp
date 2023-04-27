a9ecb2aec8bd9496e8fa34bb9a641ad23b5900d7
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-28 00:02:49 +0530

    io header file

---
+++ src/io/io.hh
@@ -0,0 +1,17 @@
+#ifndef IO_HH
+#define IO_HH
+
+#include <openutils/sstring/sstring.hh>
+#include <sys/stat.h>
+
+namespace runcpp
+{
+    class io
+    {
+      public:
+        static openutils::sstring read_file(const openutils::sstring &loc);
+        static bool save_file(const openutils::sstring &loc, const openutils::sstring &content);
+        static bool file_exists(const openutils::sstring &loc);
+    };
+}
+#endif
