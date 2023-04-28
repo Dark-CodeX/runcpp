64b0ca45f40add2fd1f8bf1f6833b4a38d2699e5
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-28 22:29:35 +0530

    `import_helper` function

--- src/parser/parser.hh
+++ src/parser/parser.hh
@@ -4,7 +4,7 @@
 #include <openutils/sstring/sstring.hh>
 #include <openutils/vector/vector.hh>
 #include <unordered_map>
-
+#include "../io/io.hh"
 #include <iostream>
 
 namespace runcpp
@@ -12,9 +12,10 @@ namespace runcpp
     class parser
     {
       private:
-        openutils::sstring _M_content, _M_location;
+        openutils::sstring _M_content, _M_location, _M_curr_location, _M_prev_location;
         std::unordered_map<openutils::heap_pair<openutils::sstring, bool>, openutils::vector_t<openutils::heap_pair<openutils::sstring, openutils::vector_t<openutils::sstring>>>> _M_map;
         static void draw_error(const openutils::sstring &loc, const openutils::sstring &line, const openutils::sstring &err_msg, const openutils::sstring &expected, std::size_t line_no, std::size_t curr_lexer, const openutils::heap_pair<openutils::sstring, openutils::lexer_token> *lexer_curr);
+        void import_helper();
 
       public:
         parser(const openutils::sstring &content, const openutils::sstring &loc);
