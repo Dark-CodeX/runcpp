534ce643819c2532d5edf6ead5653f24243482da
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-05-15 23:31:48 +0530

    made error messages cleaner

--- src/caller/caller.cc
+++ src/caller/caller.cc
@@ -31,11 +31,11 @@ namespace runcpp
             waitpid(this->pid, &status, 0);
             if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS)
             {
-                std::fprintf(stderr, "err: child process exited with status `%d`\n", WEXITSTATUS(status));
+                std::fprintf(stderr, "err: child process exited with status %d\n", WEXITSTATUS(status));
             }
             else if (WIFSIGNALED(status))
             {
-                std::fprintf(stderr, "err: child process terminated by signal `%d`\n", WTERMSIG(status));
+                std::fprintf(stderr, "err: child process terminated by signal %d\n", WTERMSIG(status));
             }
         }
     }
@@ -61,7 +61,7 @@ namespace runcpp
 
         if (!CreateProcess(nullptr, (LPTSTR)(this->cmds.c_str()), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &this->si, &this->pi))
         {
-            std::fprintf(stderr, "err: CreateProcess failed and returned error code `%lu`.\r\n", GetLastError());
+            std::fprintf(stderr, "err: CreateProcess failed and returned error code %lu.\r\n", GetLastError());
             std::exit(EXIT_FAILURE);
         }
 
