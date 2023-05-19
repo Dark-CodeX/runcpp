80d67e347920a8a63463132a6ed8fcff2d45924d
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-05-19 11:01:17 +0530

    hashes of used keywords generated using `sstring::hash()`

---
+++ src/parser/used_keywords.h
@@ -0,0 +1,31 @@
+#ifndef USED_KEYWORDS_H
+#define USED_KEYWORDS_H
+
+/**
+ * @brief Do NOT change these values manually, instead use `sstring::hash()` function to generate these hashes.
+ */
+static const std::size_t used_keywords[] =
+    {
+        475434153, //--help
+        688856826, //-h
+        651722557, //--libs
+        688857038, //-l
+        581504501, //--version
+        688857568, //-v
+        168229698, //--generate
+        690955096, //-gen
+        875873665, //--file
+        688856720, //-f
+        904548199, //--print
+        688857250, //-p
+        688857197, //-o
+        74350762,  //--serialize
+        688857409, //-s
+        550874821, //--deserialize
+        688856614, //-d
+        125887366, //--init
+        688856879, //-i
+        40457,     // run
+};
+
+#endif
