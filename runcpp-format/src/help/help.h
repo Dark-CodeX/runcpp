0d82c2eac219f306d2537f386c37dcdc34a31386
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-05-24 19:36:28 +0530

    removed extra line character

--- runcpp-format/src/help/help.h
+++ runcpp-format/src/help/help.h
@@ -5,7 +5,7 @@ static const char *runcpp_format_help = "Usage: [files or options]\n"
                                  "Options:\n"
                                  "    --help, -h                Display this help message.\n"
                                  "    --libs, -l                Show the list of used libraries.\n"
-                                 "    --version, -v             Display the version of the app.\n";
+                                 "    --version, -v             Display the version of the app.";
 
 static const char *runcpp_format_libs = "Copyright 2023 Tushar Chaurasia\n"
                                         "    1. [sstring v" OPENUTILS_sstring_version "]      (https://github.com/Dark-CodeX/sstring.git)        Tushar Chaurasia\n"
