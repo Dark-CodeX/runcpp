0175ee9de83465336f595d61c38cda52ea067883
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-06-28 21:22:16 +0530

    using c++ wrapper instead of plain c

--- runcpp/src/os/os.cc
+++ runcpp/src/os/os.cc
@@ -1,29 +1,32 @@
 /**
- * @file os.c
+ * @file os.cc
  * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
  * @author Tushar Chaurasia (Dark-CodeX)
  */
 
-#include "./os.h"
+#include "./os.hh"
 
-const char *get_os(void)
+namespace runcpp
 {
+    const char *os::get_os(void)
+    {
 #if defined __linux__ || defined linux || defined __linux
-    return "linux";
+        return "linux";
 #elif _WIN32 || defined _WIN64 || defined __CYGWIN__
-    return "windows";
+        return "windows";
 #elif defined __unix__ || defined __unix || defined unix
-    return "unix";
+        return "unix";
 #elif defined __APPLE__ || defined __MACH__
-    return "mac";
+        return "mac";
 #endif
-}
+    }
 
-unsigned int get_app_arch(void)
-{
+    unsigned int os::get_app_arch(void)
+    {
 #if defined __x86_64__ || defined __ppc64__
-    return 64;
+        return 64;
 #else
-    return 32;
+        return 32;
 #endif
+    }
 }
--- runcpp/src/os/os.hh
+++ runcpp/src/os/os.hh
@@ -1,13 +1,20 @@
 /**
- * @file os.h
+ * @file os.hh
  * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
  * @author Tushar Chaurasia (Dark-CodeX)
  */
 
-#ifndef RUNCPP_OS_H
-#define RUNCPP_OS_H
+#ifndef RUNCPP_OS_HH
+#define RUNCPP_OS_HH
 
-const char *get_os(void);
-unsigned int get_app_arch(void);
+namespace runcpp
+{
+    class os
+    {
+    public:
+        static const char *get_os(void);
+        static unsigned int get_app_arch(void);
+    };
+}
 
 #endif
