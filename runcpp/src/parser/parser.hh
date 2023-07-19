0dfd6a412c293ab081ca8692c3eac5a67be80be2
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-07-19 16:57:16 +0530

    more spaces between functions

--- runcpp/src/parser/parser.hh
+++ runcpp/src/parser/parser.hh
@@ -31,15 +31,18 @@ namespace runcpp
         // This variable stores the parsed data, here I am using `std::size_t` instead of `openutils::sstring` for KEY, to improve performance and reduce memory usage
         std::unordered_map<std::size_t, openutils::vector_t<openutils::vector_t<openutils::sstring>>> M_map;
 
-        openutils::vector_t<openutils::sstring> M_target_vector;                      // this variable stores target's name only IF '--print-gui-client' flag is used
-        openutils::sstring M_lable;                                                   // current target's name
+        openutils::vector_t<openutils::sstring> M_target_vector; // this variable stores target's name only IF '--print-gui-client' flag is used
+
+        openutils::sstring M_lable; // current target's name
+
         openutils::vector_t<openutils::vector_t<openutils::sstring>> M_adding_vector; // this vector stores the list of (value of a variable) in a target
 
         std::size_t M_curr_line; // current line
 
         bool store_target_names; // decideds whether 'M_target_vector' will store names or not
 
-        bool M_was_if_true;   // was condition of if true and does code under this block will execute
+        bool M_was_if_true; // was condition of if true and does code under this block will execute
+
         bool M_was_else_true; // if true means if's condition was false, hence run only else block's code
 
         enum BLOCK_TYPE
@@ -56,8 +59,6 @@ namespace runcpp
 
         [[nodiscard]] static openutils::vector_t<openutils::sstring> convert_2d_vec_to_1d(const openutils::vector_t<openutils::vector_t<openutils::sstring>> &data);
 
-        [[nodiscard]] static bool evaluate_ifs(const openutils::vector_t<openutils::sstring> &lhs, const openutils::vector_t<openutils::sstring> &rhs);
-
         [[nodiscard]] static bool is_used_keyword(const std::size_t &__keyword_hash);
 
     private: // private static helper functions
@@ -71,18 +72,30 @@ namespace runcpp
 
         [[nodiscard]] static bool helper_parsing(parser &ps, const openutils::sstring &content, const unsigned int &lines_to_read);
 
+        [[nodiscard]] static bool evaluate_ifs(const openutils::vector_t<openutils::sstring> &lhs, const openutils::vector_t<openutils::sstring> &rhs);
+
+        [[nodiscard]] static openutils::sstring validate_quotes(parser &ps, const openutils::sstring &quote_type, const openutils::vector_t<openutils::heap_pair<openutils::sstring, enum openutils::lexer_token>> &lexer, std::size_t &j);
+
     public: // deleted functions
         parser(const parser &) = delete;
+
         parser(parser &&) = delete;
+
         parser &operator=(const parser &) = delete;
+
         parser &operator=(parser &&) = delete;
 
     public: // real functions
         parser() = default;
+
         parser(const openutils::sstring &location, bool store_target = false);
+
         [[nodiscard]] bool perform_parsing(const unsigned int &max_lines);
+
         [[nodiscard]] openutils::vector_t<openutils::sstring> generate_command(const openutils::sstring &__key) const;
+
         [[nodiscard]] const openutils::vector_t<openutils::vector_t<openutils::sstring>> &generate_commands_all() const;
+
         [[nodiscard]] bool contains_key(const openutils::sstring &__key) const;
 
         /** @brief NOTE: this function does not clears unordered_map's data*/
@@ -95,6 +108,7 @@ namespace runcpp
         [[nodiscard]] bool merge(const openutils::sstring &out) const;
 
         [[nodiscard]] static bool serialize(const parser &p, const openutils::sstring &location);
+
         [[nodiscard]] static bool deserialize(parser &p, const openutils::sstring &location);
     };
 }
