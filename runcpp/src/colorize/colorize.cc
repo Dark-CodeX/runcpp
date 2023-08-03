5d86f96297599ab3545f5506fa3a3cd6b99bdee3
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-08-03 15:17:32 +0530

    checks if stdout and stderr are printed on terminal or not

---
+++ runcpp/src/colorize/colorize.cc
@@ -0,0 +1,24 @@
+/**
+ * @file colorize.cc
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#include "./colorize.hh"
+
+namespace runcpp
+{
+    bool colorize::should_colorize(const colorize::STDPTR &fl)
+    {
+#if defined _WIN32 || defined _WIN64 || defined __CYGWIN__
+        HANDLE h;
+        DWORD m;
+
+        h = GetStdHandle((fl == colorize::STDPTR::OUT ? STD_OUTPUT_HANDLE : STD_ERROR_HANDLE));
+        return (h != INVALID_HANDLE_VALUE) && (h != NULL) && GetConsoleMode(h, &m);
+#else
+        char const *t = std::getenv("TERM");
+        return t && std::strcmp(t, "dumb") != 0 && isatty((fl == colorize::STDPTR::OUT ? STDOUT_FILENO : STDERR_FILENO));
+#endif
+    }
+}
---
+++ runcpp/src/colorize/colorize.hh
@@ -0,0 +1,33 @@
+/**
+ * @file colorize.hh
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
+#ifndef RUNCPP_COLORIZE_HH
+#define RUNCPP_COLORIZE_HH
+
+#if defined _WIN32 || defined _WIN64 || defined __CYGWIN__
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+
+namespace runcpp
+{
+    class colorize
+    {
+    public:
+        enum class STDPTR : unsigned char
+        {
+            OUT,
+            ERR
+        };
+        [[nodiscard]] static bool should_colorize(const STDPTR &fl);
+    };
+}
+
+#endif
